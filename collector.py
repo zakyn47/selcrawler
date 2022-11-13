e58c6132a23f0209ac4fa4c163f20dcbd6f8ef83
zakyn47 <zalcik.lukas@seznam.cz>
2022-11-13 21:07:33 +0100

    autopep

--- collector.py
+++ collector.py
@@ -1,26 +1,27 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import re
-from locators import Locators, AlzaLocators
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from locators import AlzaLocators, Locators
 
 
 class Collector(object):
 
     email_regex = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
     phone_regex = re.compile(r"(^\+?[\d\s?]{10,15})")
-    
-    urls = []
 
+    urls = []
 
     def log_this(func):
         """Decorator for data logging."""
+
         def wrapper(self):
             with open("log.txt", "a") as f:
                 for element in func(self):
                     f.write(element + "\n")
         return wrapper
-
 
     @log_this
     def get_emails(self) -> list:
@@ -30,30 +31,27 @@ class Collector(object):
             emails = self.email_regex.findall(element.text)
         return emails
 
-
     @log_this
     def get_phones(self) -> list:
         """Returns list of all phone numbers on the page."""
         print("collecting phones at page: " + self.driver.current_url)
         for element in self.all_page_elements():
             phones = self.phone_regex.findall(element.text)
-        return phones            
-                
-
+        return phones
 
     def all_page_elements(self) -> list:
         """Returns list of all elements on the page."""
         WebDriverWait(self.driver, 10) \
-        .until(EC.presence_of_all_elements_located(Locators.BODY))
+            .until(EC.presence_of_all_elements_located(Locators.BODY))
         return self.driver.find_elements(*Locators.BODY)
-
 
     @log_this
     def get_urls(self) -> list:
         """Returns list of all urls on the page."""
         print("collecting urls at page: " + self.driver.current_url)
         urls = []
-        url_links = self.driver.find_element(*Locators.BODY).find_elements(By.TAG_NAME, "a")
+        url_links = self.driver.find_element(
+            *Locators.BODY).find_elements(By.TAG_NAME, "a")
         try:
             for link in url_links:
                 if link.get_attribute("href").startswith("http"):
@@ -63,11 +61,12 @@ class Collector(object):
             pass
         return urls
 
-
     # alza
+
     def alza_items(self):
         print("collecting items at page: " + self.driver.current_url)
-        items = self.driver.find_element(*AlzaLocators.ITEMS).find_elements(By.TAG_NAME, "a")
+        items = self.driver.find_element(
+            *AlzaLocators.ITEMS).find_elements(By.TAG_NAME, "a")
         print(len(items))
         for item in items:
             print(item.text)
--- crawler.py
+++ crawler.py
@@ -1,36 +1,34 @@
+import random
+import time
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+
 from collector import Collector
-import random
 
 
 class Crawler(Collector):
 
     urls = []
-     
 
     def __init__(self, url: str):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.wait = WebDriverWait(self.driver, 10)
         self.driver.implicitly_wait(3)
         self.driver.get(url)
-    
 
     def go_to_next_url(self):
         if len(self.urls) > 1:
             self.driver.get(random.choice(self.urls))
-    
 
     def close(self):
         self.driver.close()
-        
+
 
 if __name__ == "__main__":
     crawler = Crawler("https://www.seznam.cz")
     start_time = time.time()
-
 
     next_pages = 5
     while next_pages > 0:
@@ -38,9 +36,6 @@ if __name__ == "__main__":
         crawler.get_urls()
         crawler.go_to_next_url()
 
-
     end_time = time.time()
     print(f"Time taken: {end_time - start_time}")
     crawler.close()
-
-
